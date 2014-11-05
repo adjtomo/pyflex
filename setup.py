d8b0666bbe3c4c1fc504e09e79c24afd0ffc0036
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-11-05 13:01:09 +0100

    Initial structure.

---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf8 -*-
+import glob
+import io
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+setup(
+    name="pyflex",
+    version="0.0.1a",
+    license="CeCILL License (version 2) ",
+    description="Experimental Python Flexwin port",
+    author="Lion Krischer, Federica Magnoni, Emanuele Casarotti, Alessia Maggi",
+    author_email="krischer@geophysik.uni-muenchen.de",
+    url="https://github.com/krischer/pyflex",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Utilities",
+    ],
+    keywords=[
+        # eg: "keyword1", "keyword2", "keyword3",
+    ],
+    install_requires=[
+        "obspy"
+    ],
+    extras_require={
+        # eg: 'rst': ["docutils>=0.11"],
+    }
+)

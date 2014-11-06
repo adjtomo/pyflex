3e3439e8d1bd0931c5e79ef2899f182f372c0697
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-11-06 17:33:18 +0100

    Refactoring and moving functionality elsewhere.

--- src/pyflex/flexwin.py
+++ src/pyflex/flexwin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Convenience functions.
 
 :copyright:
     Lion Krischer (krischer@geophysik.uni-muenchen.de), 2014
@@ -12,83 +13,21 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.builtins import *  # NOQA
 
-import collections
-import numpy as np
 
-import obspy
-
-from . import PyflexError
-
-import warnings
-
-Event = collections.namedtuple("Event",
-                               ["latitude", "longitude", "depth_in_m"])
-
-Station = collections.namedtuple("Station", ["latitude", "longitude"])
+from .window_selector import WindowSelector
 
 
-def select_windows(observed, synthetic, min_period, max_period):
+def select_windows(observed, synthetic, config):
     """
-    Function picking windows.
+    Convenience function for picking windows.
 
-    :param observed: A preprocessed :class:`~obspy.core.trace.Trace` object
-        containing the observed data.
-    :param observed: A preprocessed :class:`~obspy.core.trace.Trace` object
+    :param observed: A trace with the observed data.
+    :type observed: :class:`~obspy.core.trace.Trace`
+    :param synthetic: A preprocessed :class:`~obspy.core.trace.Trace` object
         containing the synthetic data.
-    :param min_period: The minimum period of the already processed data and
-        synthetics.
-    :param max_period: The maximum period of the already processed data and
-        synthetics.
+    :type synthetic: :class:`~obspy.core.trace.Trace`
+    :param config: Configuration object.
+    :type config: :class:`~.config.Config`
     """
-    # Copy to not modify the original data.
-    observed = observed.copy()
-    synthetic = synthetic.copy()
-    _sanity_checks(observed, synthetic)
-
-
-
-def _sanity_checks(observed, synthetic):
-    """
-    Perform a number of basic sanity checks to assure the data is valid in a
-    certain sense.
-
-    It checks the types of both, the starttime, sampling rate, number of
-    samples, ...
-
-    :param observed:
-    :param synthetic:
-    :return:
-    """
-    if not isinstance(observed, obspy.Trace):
-        raise PyflexError("Observed data must be an ObsPy Trace object.")
-    if not isinstance(synthetic, obspy.Trace):
-        raise PyflexError("Synthetic data must be an ObsPy Trace object.")
-
-    if observed.stats.npts != synthetic.stats.npts:
-        raise PyflexError("Observed and synthetic data must have the same "
-                          "number of samples.")
-
-    sr1 = observed.stats.sampling_rate
-    sr2 = synthetic.stats.sampling_rate
-
-    if abs(sr1 - sr2) / sr1 >= 1E-5:
-        raise PyflexError("Observed and synthetic data must have the same "
-                          "sampling rate.")
-
-    if observed.stats.starttime != synthetic.stats.starttime:
-        raise PyflexError("Observed and synthetic data must have the same "
-                          "starttime.")
-
-    ptp = sorted([observed.data.ptp(), synthetic.data.ptp()])
-    if ptp[1] / ptp[0] >= 5:
-        warnings.warn("The amplitude difference between data and synthetic "
-                      "is fairly large.")
-
-    # Also check the components of the data to avoid silly mistakes of users.
-    if len(set(observed.stats.channel[-1].upper(),
-               synthetic.stats.channel[-1].upper())) != 1:
-        warnings.warn("The orientation code of synthetic and observed data "
-                      "is not equal.")
-
-    observed.data = np.ascontiguousarray(observed.data, dtype=np.float64)
-    synthetic.data = np.ascontiguousarray(synthetic.data, dtype=np.float64)
+    ws = WindowSelector(observed=observed, synthetic=synthetic, config=config)
+    return ws.launch_selection()
--- src/pyflex/window_selector.py
+++ src/pyflex/window_selector.py
@@ -17,28 +17,26 @@ with standard_library.hooks():
     import itertools
 
 import numpy as np
+import obspy
+import warnings
 
-from . import utils
+from . import PyflexError, utils
 from .stalta import sta_lta
 from .window import Window
 
 
 class WindowSelector(object):
-    def __init__(self, observed, synthetic, dt, config):
+    def __init__(self, observed, synthetic, config):
         self.observed = observed
         self.synthetic = synthetic
-        self.dt = dt
         self.config = config
 
         self.windows = []
 
-        self.stalta = sta_lta(np.abs(self.synthetic), self.dt,
+    def launch_selection(self):
+        self.stalta = sta_lta(np.abs(self.synthetic), self.observed.stats.dt,
                               self.config.min_period)
         self.peaks, self.troughs = utils.find_local_extrema(self.stalta)
-
-        self.launch_selection()
-
-    def launch_selection(self):
         self.initial_window_selection()
 
     def initial_window_selection(self):
@@ -74,17 +72,42 @@ class WindowSelector(object):
     def reject_windows_based_on_minimum_length(self, min_length):
         self.windows = list(itertools.filter(
             lambda x: (x.right - x.left) > min_length,  self.windows))
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-The same STA/LTA as used in Flexwin.
 
-:copyright:
-    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2014
-:license:
-    GNU General Public License, Version 3
-    (http://www.gnu.org/copyleft/gpl.html)
-"""
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-from future.builtins import *  # NOQA
+    def _sanity_checks(self):
+        """
+        Perform a number of basic sanity checks to assure the data is valid in a
+        certain sense.
+
+        It checks the types of both, the starttime, sampling rate, number of
+        samples, ...
+        """
+        if not isinstance(self.observed, obspy.Trace):
+            raise PyflexError("Observed data must be an ObsPy Trace object.")
+        if not isinstance(self.synthetic, obspy.Trace):
+            raise PyflexError("Synthetic data must be an ObsPy Trace object.")
+
+        if self.observed.stats.npts != self.synthetic.stats.npts:
+            raise PyflexError("Observed and synthetic data must have the same "
+                              "number of samples.")
+
+        sr1 = self.observed.stats.sampling_rate
+        sr2 = self.synthetic.stats.sampling_rate
+
+        if abs(sr1 - sr2) / sr1 >= 1E-5:
+            raise PyflexError("Observed and synthetic data must have the same "
+                              "sampling rate.")
+
+        if self.observed.stats.starttime != self.stats.starttime:
+            raise PyflexError("Observed and synthetic data must have the same "
+                              "starttime.")
+
+        ptp = sorted([self.observed.data.ptp(), self.synthetic.data.ptp()])
+        if ptp[1] / ptp[0] >= 5:
+            warnings.warn("The amplitude difference between data and synthetic "
+                          "is fairly large.")
+
+        # Also check the components of the data to avoid silly mistakes of users.
+        if len(set(self.observed.stats.channel[-1].upper(),
+                   self.synthetic.stats.channel[-1].upper())) != 1:
+            warnings.warn("The orientation code of synthetic and observed data "
+                          "is not equal.")

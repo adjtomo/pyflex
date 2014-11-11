b2c715cb40966aa1541ba6efc9286ca36d00b029
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-11-11 17:16:55 +0100

    New minor release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pyflex",
-    version="0.1.0",
+    version="0.1.1",
     license='GNU General Public License, Version 3 (GPLv3)',
     description="Python port of the FLEXWIN package",
     author="Lion Krischer",

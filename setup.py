697710d3509fae6e784115c3b3c176bdd5f5f4fe
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-01-27 11:56:58 +0100

    Updating classifiers

--- setup.py
+++ setup.py
@@ -45,8 +45,9 @@ setup(
     classifiers=[
         # complete classifier list:
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "Operating System :: Unix",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
@@ -57,7 +58,9 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ],
     keywords=[
         "seismology", "flexwin", "science", "tomography", "inversion"
